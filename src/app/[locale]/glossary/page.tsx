4f46f4706ae437b5cdd20a2b573967815abf9971
losnah-think <hansol416@tu.ac.kr>
2025-11-07 17:35:03 +0900

    Update GNB menu text from '용어 설명' to '기능 정의서' in all language files

--- src/app/[locale]/glossary/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import { WarehouseDashboard, warehouseData } from '@/components/WarehouseVisualization';
-
-export default function GlossaryRoute() {
-  return <WarehouseDashboard warehouse={warehouseData} />;
-}
